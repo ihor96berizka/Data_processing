d041641d1b8f23dc8635bcefc4cd3012b3f0f738
Ihor Berizka <ihor.berizka@gmail.com>
2019-10-06 23:10:56 +0300

    removed duplicates from data

--- parse_csv.py
+++ parse_csv.py
@@ -5,27 +5,40 @@ from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 
-data = pandas.read_csv('august2.csv', parse_dates=['hour'])
+filename = 'september.csv'
+data = pandas.read_csv(filename, parse_dates=['hour'])
 
 n = 1000
-dates = data['hour'][0:n]
+dates = data['hour'].to_list()#[0:n]
+print('type(dates): ', type(dates))
+print('dates: ')
 print(dates)
 
-temperature = data['temperature'][0:n]
-for i in range(0,n):
-    temperature[i] = temperature[i] / 100.0
+temperature = data['temperature'].to_list()#[0:n]
+#for i in range(0,len(dates)):
+#    temperature[i] = temperature[i] / 100.0
+print('type(temperature): ', type(temperature))
+print('temperature: ')
 print(temperature)
 
-depth = data['depth'][0:n]
+filteredData = dict(zip(dates, temperature))
+print('filteredData:')
+print(filteredData)
+# A list of the keys of dictionary
+list_keys = [k for k in filteredData]
 
-plt.subplot(2, 1, 1)
-plt.plot(dates, temperature, '.')
+# or a list of the values
+list_values = [v for v in filteredData.values()]
+#depth = data['depth'][0:n]
+
+plt.subplot(1, 1, 1)
+plt.plot(list_keys, list_values, '-o')
 plt.title('Sensors readings')
 plt.ylabel('temperature, C')
-
+'''
 plt.subplot(2, 1, 2)
-plt.plot(dates, depth, '.')
+plt.plot(dates, temperature, '.')
 plt.ylabel("Depth, cm")
 plt.xlabel('date')
-
+'''
 plt.show()
--- rest_api_test.py
+++ rest_api_test.py
@@ -1,10 +1,63 @@
 import requests
+import json
 
-payload = {'device': 'Il/C', 'startDate': '2019-09-29', 'endDate': '2019-10-1', 'properties': 'depth'}
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data_2019-9-01_to_2019-9-30.json'
+
+payload = {'device': 'Il/C', 'startDate': '2019-9-01', 'endDate': '2019-9-30', 'properties': 'temperature'}
 response = requests.get("https://yz58i1wove.execute-api.eu-west-1.amazonaws.com"
-                 "/dev/data/group-by/minute", params=payload)
+                "/dev/data/group-by/hour", params=payload)
 
 print('response info')
 print(response.url)
 print(response.status_code)
-print(response.json())
+jsonRecv = response.json()
+
+print('save json to file')\
+
+with open(filename, "w") as write_file:
+    json.dump(jsonRecv, write_file)
+
+#print(jsonRecv)
+'''
+with open(filename, "r") as read_file:
+    jsonRecv = json.load(read_file)
+
+print('jsonRecv type: ', type(jsonRecv))
+'''
+dates = []
+temperature = []
+
+for item in jsonRecv:
+    date = datetime.strptime(item['hour'], '%Y-%m-%d %H:%M:%S.%f')
+    dates.append(date)
+    temperature.append(float(item['temperature']) / 100.0)
+    #print('date: ', item['hour'])
+
+print('len(dates)= ', len(dates))
+print('type(dates) = ', type(temperature[0]))
+
+data = dict(zip(dates, temperature))
+print('dict: ')
+print(data)
+print('items in dict: ')
+print(len(data))
+
+# A list of the keys of dictionary
+list_keys = [k for k in data]
+
+# or a list of the values
+list_values = [v for v in data.values()]
+
+
+def plotting():
+    plt.subplot(1, 1, 1)
+    plt.plot(list_keys, list_values)
+    plt.title('Sensors readings')
+    plt.ylabel('temperature, C')
+    plt.show()
+
+
+plotting()
